3b7068756bc675c85564753eb935eda05e75dd98
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-11 20:58:56 +0900

    feat: 보관함 chip 추가

---
+++ app/storage/FestivalChip.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+import bookmark from "@/app/assets/bookmark.svg";
+import festivalItemImg from "@/app/assets/curation1.jpg";
+
+const FestivalChip = () => {
+  return (
+    <div className={styles.chip}>
+      <Image
+        className={styles.bookmark}
+        src={bookmark}
+        alt="bookmark"
+        width={25}
+        height={25}
+      />
+      <Image src={festivalItemImg} alt="bookmark" width={270} height={300} />
+      <div>
+        <h4>축제명</h4>
+        <h5>위치</h5>
+      </div>
+    </div>
+  );
+};
+
+export default FestivalChip;
--- app/storage/page.tsx
+++ app/storage/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./index.module.css";
+import FestivalChip from "./FestivalChip";
 
 function StoragePage() {
   return (
     <main>
-      <div>보관함</div>
+      <div className={styles.container}>
+        <FestivalChip />
+        <FestivalChip />
+        <FestivalChip />
+        <FestivalChip />
+      </div>
     </main>
   );
 }

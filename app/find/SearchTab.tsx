de93f9c14c87e1711ddeadc2249dac67d4f8efa5
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-11 12:23:11 +0900

    feat: 발견 검색 input 추가

--- app/find/SearchTab.tsx
+++ app/find/SearchTab.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import styles from "./index.module.css";
+import Image from "next/image";
+import search_blue from "@/app/assets/search_blue.svg";
 
 const SearchTab = () => {
   return (
-    <div>
-      <input />
-      <div>검색</div>
+    <div className={styles.search_tab_container}>
+      <input placeholder="어떤 축제에 가고 싶어?" />
+      <Image src={search_blue} alt="Fiesta Flow logo" width={25} height={25} />
     </div>
   );
 };

08ae0d7a65abe41ebf3d83ddc0ac143e5f742c48
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-13 08:09:36 +0900

    feat: 온보딩 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import styles from "./index.module.css";
+import splash from "@/app/assets/splash.jpg";
 
 function HomePage() {
   return (
     <>
-      <div>Fiesta Flow</div>
-      <Link href="/home" className="white_button">
+      <Image
+        src={splash}
+        alt="온보딩 이미지"
+        height={700}
+        className={styles.splash_image}
+      />
+      <div className={styles.logo}>Fiesta Flow</div>
+      <Link href="/home" className={`white_button`}>
         시작하기
       </Link>
     </>

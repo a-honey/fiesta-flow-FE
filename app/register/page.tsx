9aee922bf0f33ad209c0184ef6723b851ee850e9
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-13 16:42:29 +0900

    chore: 회원가입 페이지 스타일 추가

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Image from "next/image";
 import React, { useState } from "react";
-import arrow_left from "@/app/assets/arrow_left.svg";
+import styles from "./index.module.css";
 
 function RegisterPage() {
   const [registerFormBody, setRegisterFormBody] = useState({
@@ -12,30 +11,29 @@ function RegisterPage() {
   });
 
   return (
-    <>
-      <Image src={arrow_left} alt="Fiesta Flow logo" width={25} height={25} />
-      <form>
+    <form className={styles.container}>
+      <div className={styles.item_container}>
         <label>이메일 주소를 입력해주세요</label>
         <input type="text" />
         <span>이메일 주소를 다시 확인해주세요</span>
-        {registerFormBody.email && (
-          <>
-            <label>비밀번호를 입력해주세요</label>
-            <input type="password" placeholder="비밀번호 입력" />
-            <span>6자 이상 입력해주세요</span>
-            <input type="password" placeholder="비밀번호 확인" />
-            <span>비밀번호와 동일하게 입력해주세요</span>
-          </>
-        )}
-        {registerFormBody.password && (
-          <>
-            <label>이름을 설정해주세요</label>
-            <input type="text" placeholder="이름" />
-          </>
-        )}
-      </form>
-      <button>다음</button>
-    </>
+      </div>
+      {registerFormBody.email && (
+        <>
+          <label>비밀번호를 입력해주세요</label>
+          <input type="password" placeholder="비밀번호 입력" />
+          <span>6자 이상 입력해주세요</span>
+          <input type="password" placeholder="비밀번호 확인" />
+          <span>비밀번호와 동일하게 입력해주세요</span>
+        </>
+      )}
+      {registerFormBody.password && (
+        <>
+          <label>이름을 설정해주세요</label>
+          <input type="text" placeholder="이름" />
+        </>
+      )}
+      <button className="white_button">다음</button>
+    </form>
   );
 }
 

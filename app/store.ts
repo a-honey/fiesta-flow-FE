dd87c275205bd0aea348341b57ae6290281a11e0
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-13 16:57:13 +0900

    feat: zustand 설치

---
+++ app/store.ts
@@ -0,0 +1,22 @@
+import create from "zustand";
+
+interface AuthState {
+  isLoggedIn: boolean;
+  id: string | null;
+  profile: string | null;
+  name: string | null;
+  login: (id: string, profile: string, name: string) => void;
+  logout: () => void;
+}
+
+const useAuthStore = create<AuthState>((set) => ({
+  isLoggedIn: false,
+  login: (id: string, profile: string, name: string) =>
+    set({ isLoggedIn: true, id, profile, name }),
+  logout: () => set({ isLoggedIn: false, id: null, profile: null, name: null }),
+  id: null,
+  profile: null,
+  name: null,
+}));
+
+export default useAuthStore;

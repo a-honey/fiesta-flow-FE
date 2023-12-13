8dfa270b00fac4463e94c71fad76b74ea82a9ae4
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-13 12:41:11 +0900

    feat: 온보딩 페이지

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,22 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./index.module.css";
 import splash from "@/app/assets/splash.jpg";
+import logo_text from "@/app/assets/logo_text.svg";
 
 function HomePage() {
   return (
-    <>
+    <section className={styles.black}>
       <Image
         src={splash}
         alt="온보딩 이미지"
-        height={700}
+        height={800}
         className={styles.splash_image}
       />
-      <div className={styles.logo}>Fiesta Flow</div>
+      <Image src={logo_text} alt="온보딩 이미지" className={styles.logo} />
       <Link href="/home" className={`white_button`}>
         시작하기
       </Link>
-    </>
+    </section>
   );
 }
 

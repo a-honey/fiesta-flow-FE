ae057c6ed6ccc04e0b7b5cfed068022934afbdab
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-13 16:37:08 +0900

    design: 로그인 페이지 디자인

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Image from "next/image";
 import React, { useState } from "react";
-import arrow_left from "@/app/assets/arrow_left.svg";
+import styles from "./index.module.css";
 import AlertBox from "./AlertBox";
 
 function LoginPage() {
@@ -14,18 +13,21 @@ function LoginPage() {
     console.log(loginFormBody);
   };
   return (
-    <>
-      <Image src={arrow_left} alt="뒤로가기" width={25} height={25} />
-      <form onSubmit={handleSubmit}>
-        <h3>로그인</h3>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <h3>로그인</h3>
+      <div className={styles.item_container}>
         <input type="text" />
         <span>이메일 주소를 다시 확인해주세요</span>
+      </div>
+      <div className={styles.item_container}>
         <input type="text" />
         <span>비밀번호를 다시 확인해주세요</span>
-        <button type="submit">다음</button>
-        <AlertBox content="해당 사용자를 찾을 수 없습니다" />
-      </form>
-    </>
+      </div>
+      <button className="white_button" type="submit">
+        확인
+      </button>
+      <AlertBox content="해당 사용자를 찾을 수 없습니다" />
+    </form>
   );
 }
 

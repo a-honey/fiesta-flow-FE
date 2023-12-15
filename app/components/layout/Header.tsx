3f32206fc613804088dbc294af3c2e035b64a513
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-16 04:41:16 +0900

    feat: zustand 로그인 상태에 따른 페이지 이동 및 프로필

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./Header.module.css";
 import notification from "@/app/assets/notification.svg";
-import profile from "@/app/assets/profile.svg";
-import Link from "next/link";
+import profile_svg from "@/app/assets/profile.svg";
+import login_svg from "@/app/assets/login.svg";
+import useAuthStore from "@/app/store";
 
 const Header = () => {
+  const { isLoggedIn, profile, name } = useAuthStore((state) => ({
+    isLoggedIn: state.isLoggedIn,
+    profile: state.profile,
+    name: state.name,
+  }));
+
   return (
     <header className={styles.container}>
       <Image
@@ -14,9 +23,25 @@ const Header = () => {
         width={18}
         height={21.5}
       />
-      <Link href="/my">
-        <Image src={profile} alt="Fiesta Flow logo" width={25} height={25} />
-      </Link>
+      {isLoggedIn ? (
+        <Link href="/my">
+          <Image
+            src={profile_svg}
+            alt={`${name}의 프로필 이미지 및 마이페이지 바로가기`}
+            width={25}
+            height={25}
+          />
+        </Link>
+      ) : (
+        <Link href="/login">
+          <Image
+            src={login_svg}
+            alt="로그인페이지 바로가기"
+            width={25}
+            height={25}
+          />
+        </Link>
+      )}
     </header>
   );
 };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,30 +2,51 @@
 import React, { useState } from "react";
 import styles from "./index.module.css";
 import AlertBox from "./AlertBox";
+import useAuthStore from "../store";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function LoginPage() {
+  const router = useRouter();
   const [loginFormBody, setLoginFormBody] = useState({
     email: "",
     password: "",
   });
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
     console.log(loginFormBody);
+    useAuthStore.getState().login("id", "", "아현");
+    router.push("/home");
   };
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
       <h3>로그인</h3>
       <div className={styles.item_container}>
-        <input type="text" />
+        <input
+          type="text"
+          value={loginFormBody.email}
+          onChange={(e) => {
+            setLoginFormBody((prev) => ({ ...prev, email: e.target.value }));
+          }}
+        />
         <span>이메일 주소를 다시 확인해주세요</span>
       </div>
       <div className={styles.item_container}>
-        <input type="text" />
+        <input
+          type="password"
+          value={loginFormBody.password}
+          onChange={(e) => {
+            setLoginFormBody((prev) => ({ ...prev, password: e.target.value }));
+          }}
+        />
         <span>비밀번호를 다시 확인해주세요</span>
       </div>
       <button className="white_button" type="submit">
         확인
       </button>
+      <Link href="register">회원가입 하러가기</Link>
       <AlertBox content="해당 사용자를 찾을 수 없습니다" />
     </form>
   );
